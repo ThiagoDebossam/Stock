6c00ecfb1629323e40bfb3b4435187e2bf8e4aab
Thiago Nogueira <thiago.nogueira@rgsistemas.com.br>
2021-06-21 20:55:59 -0300

    Create the make of the frontend

---
+++ frotend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+      <template v-if="isLogged">
+        <v-app-bar>
+        </v-app-bar>
+        <router-view>
+        </router-view>
+      </template>
+      <template v-else>
+        <router-view>
+        </router-view>
+      </template>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      isLogged: false
+    }
+  }
+}
+</script>
